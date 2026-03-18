de83dd5f07d829c023b35235b040e99d9c50834b
jae-you <61827666+jae-you@users.noreply.github.com>
2026-03-18 15:54:14 +0900

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,16 +2,16 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: 'e20e2285ad1883f5b7c801cba35de1dc?v=61ce2285ad1882ce893d08be5ac2283e',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Brew AI Lab',
+  domain: 'www.brew.ai.kr',
+  author: 'Jae You',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
